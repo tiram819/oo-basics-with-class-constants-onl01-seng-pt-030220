0e430f2ac7f854d14f989bd8ee8e8ca0d45e4edb
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 05:32:10 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -12,3 +18,4 @@ class Shoe
   end
 
 end
+
