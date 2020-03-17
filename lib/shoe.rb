adb66658b507f4b2e09a56be717adee6e0b663d1
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 05:28:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,12 @@ class Shoe
   attr_reader :brand, 
   
   BRANDS = []
-
-   def initialize(brand)
-    @brand = brand 
-    BRANDS << brand unless
-    BRANDS.include?(brand)
+def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
     end
+  end
 
 
   
