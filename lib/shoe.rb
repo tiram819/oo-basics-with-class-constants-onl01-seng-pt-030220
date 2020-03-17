930022bec34d7478296b770bd3c9cdb54bea0f85
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 05:31:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
-      BRANDS << @brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
     end
   end
-
 
   def cobble
     self.condition = "new"
